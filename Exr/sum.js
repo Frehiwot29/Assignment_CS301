737f42079e044039879db2818975f41074037c5c
frekassa2101 <69208220+frekassa2101@users.noreply.github.com>
2021-02-22 17:32:04 -0500

    updated

---
+++ Exr/sum.js
@@ -0,0 +1,8 @@
+// let x = 9;
+// let y = 8;
+// let sum = x + y;
+// console.log(sum);
+"use strict";
+x=4;
+y=7;
+console.log(y+x);
--- w1d1/sum.js
+++
@@ -1,4 +0,0 @@
-let x = 9;
-let y = 8;
-let sum = x + y;
-console.log(sum);
