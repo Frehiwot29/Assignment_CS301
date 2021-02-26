7f546e228f7f7241802daac17ab7d46ef17c9660
frekassa2101 <69208220+frekassa2101@users.noreply.github.com>
2021-02-25 20:39:17 -0500

    W1D4 Assignment

---
+++ W1D4/factorial.js
@@ -0,0 +1,12 @@
+/**
+ * Write a Java program to calculate the factorial value of a given number. E.g.,
+ *  factorial 4 =4*3*2*1 = 24
+*/
+const prompt = require("prompt-sync")();
+let num = +prompt("Enter the number: ");
+let result = 1;
+for (let i = num; i >= 1; i--) {
+    result = i * result;
+    //console.log(result)
+}
+console.log(result)
---
+++ W1D4/isPrimeCheckBreak.js
@@ -0,0 +1,20 @@
+/**
+ * b. This time think of a logic to use break statement so that you can break early from the
+      loop, right when you know number is not prime. For example, you know number 8 is not
+     prime right on the first loop because it is perfectly divisible by 2 itself, that's why further
+     testing is not required. If the number is prime loop will proceed until the end.
+ *  
+*/
+const prompt = require("prompt-sync")();
+
+let n = prompt("Please enter a number to test if it is pime: ");
+let isPrime = true;
+
+for (let i = 2; i < n; i++) {
+    if (n % i == 0) {
+        isPrime = false;
+        break;
+    }
+}
+
+console.log(isPrime);
